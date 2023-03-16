1109f454ed81b933429c49042dea332a967697c3
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-03-16 14:49:20 +0600

    Адаптировал форму

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -5,3 +5,6 @@ class LoginForm(forms.Form):
     username = forms.CharField(required=True, label='Логин')
     password = forms.CharField(required=True, label='Пароль',
                                widget=forms.PasswordInput)
+
+    username.widget.attrs.update({'class': 'form-control form-control-lg'})
+    password.widget.attrs.update({'class': 'form-control form-control-lg'})
